8240d77693a87aa0de5e59f4c062970da5b71ffd
OdZarref <ludwigvonmarcelo@gmail.com>
2024-03-13 10:16:31 -0300

    im gonna cry

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function glitchedLetterEffect(element) {
     let interval = setInterval(() => {
         element.innerText = element.innerText
             .split('')
-            .map((letter, index) => {
-                if (iterations >= index) return element.dataset.value[index];
+            .map((_, index) => {
+                if (iterations >= index)
+                    return element.dataset.value.split(' ')[0][index];
                 else return alphabet[Math.floor(Math.random() * 26)];
             })
             .join('')
@@ -22,4 +23,38 @@ function callGlitched() {
     });
 }
 
-callGlitched();
+function backgroundTextEffect() {
+    let lastWordIndex = 0;
+
+    function clearWord(element) {
+        setInterval(() => {
+            element.innerText = element.innerText.slice(0, -1);
+        }, 500);
+    }
+
+    function writeWord(element, word) {
+        word.map((letter) => {
+            element.innerText += letter;
+        });
+    }
+
+    let element = document.querySelector('.terminal-texting');
+
+    clearWord(element);
+    console.log(element.dataset.value.split(' '));
+    setInterval(() => {
+        if (element.innerText.length == 0) {
+            console.log(element.dataset.value.split(' ')[lastWordIndex]);
+            writeWord(
+                element,
+                element.dataset.value.split(' ')[lastWordIndex].split('')
+            );
+            lastWordIndex++;
+        }
+        if (lastWordIndex == element.dataset.value.split(' ').length)
+            lastWordIndex = 0;
+    }, 100);
+}
+
+// callGlitched();
+backgroundTextEffect();
