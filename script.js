92e079dc7dc1ab6af6c5fbd6fae86b42d5b6b503
OdZarref <ludwigvonmarcelo@gmail.com>
2024-03-15 10:17:26 -0300

    Updating site

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let writting = false;
+let erasing = false;
 
 function glitchedLetterEffect(element) {
     let iterations = 0;
@@ -6,13 +8,14 @@ function glitchedLetterEffect(element) {
         element.innerText = element.innerText
             .split('')
             .map((_, index) => {
-                if (iterations >= index)
-                    return element.dataset.value.split(' ')[0][index];
+                console.log(element.dataset.value);
+                if (iterations >= index) return element.dataset.value[index];
                 else return alphabet[Math.floor(Math.random() * 26)];
             })
             .join('')
             .toUpperCase();
-        iterations += 1 / 3;
+        if (element.getAttribute('faster')) iterations += 1.5;
+        else iterations += 1 / 3;
         if (iterations >= element.dataset.value.length) clearInterval(interval);
     }, 30);
 }
@@ -24,37 +27,43 @@ function callGlitched() {
 }
 
 function backgroundTextEffect() {
-    let lastWordIndex = 0;
-
     function clearWord(element) {
+        erasing = true;
         setInterval(() => {
             element.innerText = element.innerText.slice(0, -1);
         }, 500);
+        erasing = false;
     }
 
     function writeWord(element, word) {
-        word.map((letter) => {
-            element.innerText += letter;
-        });
+        writting = true;
+        word = word.split('');
+        let index = 0;
+
+        let interval = setInterval(function () {
+            console.log(element.innerText + word[index]);
+            element.innerText = element.innerText + word[index];
+            if (index == word.length - 1) clearInterval(interval);
+            index++;
+        }, 1000);
     }
 
+    let lastWordIndex = 0;
     let element = document.querySelector('.terminal-texting');
 
-    clearWord(element);
-    console.log(element.dataset.value.split(' '));
     setInterval(() => {
-        if (element.innerText.length == 0) {
-            console.log(element.dataset.value.split(' ')[lastWordIndex]);
-            writeWord(
-                element,
-                element.dataset.value.split(' ')[lastWordIndex].split('')
-            );
+        if (element.innerText.length == 0 && writting == false) {
+            writeWord(element, element.dataset.value.split(' ')[2]);
             lastWordIndex++;
         }
+
         if (lastWordIndex == element.dataset.value.split(' ').length)
             lastWordIndex = 0;
+
+        if (erasing == false && writting == false) {
+            clearWord(element);
+        }
     }, 100);
 }
 
 callGlitched();
-backgroundTextEffect();
